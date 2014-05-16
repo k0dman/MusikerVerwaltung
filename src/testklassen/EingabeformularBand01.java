719b7fd1b18251b0f70d73b5092a179e93c504ec
brandtphl <brandt.phl@gmail.com>
2014-05-16 22:34:27 +0200

    dd

--- src/testklassen/EingabeformularBand01.java
+++ src/testklassen/EingabeformularBand01.java
@@ -16,43 +16,23 @@ public class EingabeformularBand01 extends JPanel {
 
 	// Labels
 	private JLabel ueschrift;
-	private JLabel vorname;
+
 	private JLabel name;
-	private JLabel titel;
-	private JLabel vorsatz;
-	private JLabel zusatz;
-	private JLabel geschlecht;
-	private JPanel geschlechtgruppe;
-	private JLabel dob;
-	private JLabel dod;
-	private JLabel pseudonym;
-	private JLabel instrument;
-	private JLabel solostueck;
+	private JLabel mitglied;
+	private JLabel ehemalig;
+	private JLabel stueckgruppe;
 	private JLabel referenz;
 	private JLabel fueller;
 
 	// JTextField
 	private JTextField jtfueschrift;
-	private JTextField jtfvorname;
 	private JTextField jtfname;
-	private JTextField jtftitel;
-	private JTextField jtfvorsatz;
-	private JTextField jtfzusatz;
-	private JTextField jtfgeschlecht;
-	private JTextField jtfdob;
-	private JTextField jtfdod;
-	private JTextField jtfpseudonym;
-	private JTextField jtfinstrument;
-	private JTextField jtfsolostueck;
+	private JTextField jtfmitglied;
+	private JTextField jtfehemalig;
+	private JTextField jtfstueckgruppe;
+	private JTextField jtfueller;
 	private JTextField jtfreferenz;
-
-	// JJRadioButton
-
-	private JRadioButton rbmann;
-	private JRadioButton rbfrau;
-	private JRadioButton rbkeineahnung;
-
-	private ButtonGroup auswahl;
+	
 
 	public JPanel jpmaindesc() {
 
@@ -62,45 +42,25 @@ public class EingabeformularBand01 extends JPanel {
 		ftfield = new Font(Font.SANS_SERIF, Font.BOLD + Font.ITALIC, 15);
 
 		ueschrift = new JLabel("Tragen Sie eine Band ein:");
-		vorname = new JLabel("Vorname");
 		name = new JLabel("Name");
-		titel = new JLabel("Titel");
-		vorsatz = new JLabel("Vorsatz");
-		zusatz = new JLabel("Zusatz");
-		geschlecht = new JLabel("Geschlecht");
-		dob = new JLabel("Geburstdatum");
-		dod = new JLabel("Todesdatum");
-		pseudonym = new JLabel("Pseudonym");
-		instrument = new JLabel("Instrument");
-		solostueck = new JLabel("Stueck");
+		mitglied = new JLabel("Mitglied");
+		ehemalig = new JLabel("Ehemalig");
+		stueckgruppe = new JLabel("Stueck");
 		referenz = new JLabel("Referenz");
+
 
 		jpmaindesc.add(ueschrift);
 		jpmaindesc.add(name);
-		jpmaindesc.add(vorname);
-		jpmaindesc.add(titel);
-		jpmaindesc.add(vorsatz);
-		jpmaindesc.add(zusatz);
-		jpmaindesc.add(geschlecht);
-		jpmaindesc.add(dob);
-		jpmaindesc.add(dod);
-		jpmaindesc.add(pseudonym);
-		jpmaindesc.add(instrument);
-		jpmaindesc.add(solostueck);
+		jpmaindesc.add(mitglied);
+		jpmaindesc.add(ehemalig);
+		jpmaindesc.add(stueckgruppe);
 		jpmaindesc.add(referenz);
 
 		ueschrift.setHorizontalAlignment(SwingConstants.RIGHT);
 		name.setHorizontalAlignment(SwingConstants.RIGHT);
-		vorname.setHorizontalAlignment(SwingConstants.RIGHT);
-		titel.setHorizontalAlignment(SwingConstants.RIGHT);
-		vorsatz.setHorizontalAlignment(SwingConstants.RIGHT);
-		zusatz.setHorizontalAlignment(SwingConstants.RIGHT);
-		geschlecht.setHorizontalAlignment(SwingConstants.RIGHT);
-		dob.setHorizontalAlignment(SwingConstants.RIGHT);
-		dod.setHorizontalAlignment(SwingConstants.RIGHT);
-		pseudonym.setHorizontalAlignment(SwingConstants.RIGHT);
-		instrument.setHorizontalAlignment(SwingConstants.RIGHT);
-		solostueck.setHorizontalAlignment(SwingConstants.RIGHT);
+		mitglied.setHorizontalAlignment(SwingConstants.RIGHT);
+		ehemalig.setHorizontalAlignment(SwingConstants.RIGHT);
+		stueckgruppe.setHorizontalAlignment(SwingConstants.RIGHT);
 		referenz.setHorizontalAlignment(SwingConstants.RIGHT);
 
 		ueschrift.setFont(ftfield);
@@ -117,81 +77,46 @@ public class EingabeformularBand01 extends JPanel {
 		jpmaininput.add(fueller);
 
 		// JTextFields erzeugen
+		
 
-		jtfvorname = new JTextField();
+
 		jtfname = new JTextField();
-		jtftitel = new JTextField();
-		jtfvorsatz = new JTextField();
-		jtfzusatz = new JTextField();
-		jtfgeschlecht = new JTextField();
-		geschlechtgruppe = new JPanel();
-		jtfdob = new JTextField();
-		jtfdod = new JTextField();
-		jtfpseudonym = new JTextField();
-		jtfinstrument = new JTextField();
-		jtfsolostueck = new JTextField();
+		jtfmitglied = new JTextField();
+		jtfehemalig = new JTextField();
+		jtfstueckgruppe = new JTextField();
 		jtfreferenz = new JTextField();
 
-		// Button erzeugen
-
-		rbmann = new JRadioButton("Mann");
-		rbfrau = new JRadioButton("Frau");
-		rbkeineahnung = new JRadioButton("Keine Ahnung");
-
-		// JRadioButtons ButtonGroup hinzufuegen
-
-		auswahl = new ButtonGroup();
-		auswahl.add(rbmann);
-		auswahl.add(rbfrau);
-		auswahl.add(rbkeineahnung);
-		geschlechtgruppe.add(rbmann);
-		geschlechtgruppe.add(rbfrau);
-		geschlechtgruppe.add(rbkeineahnung);
 
 		// JTextfields vergoesern
+		
 
 		jtfname.setColumns(10);
-		jtfvorname.setColumns(10);
-		jtftitel.setColumns(10);
-		jtfvorsatz.setColumns(10);
-		jtfzusatz.setColumns(10);
-
-		jtfdob.setColumns(10);
-		jtfdod.setColumns(10);
-		jtfpseudonym.setColumns(10);
-		jtfinstrument.setColumns(10);
-		jtfsolostueck.setColumns(10);
+		jtfmitglied.setColumns(10);
+		jtfehemalig.setColumns(10);
+		jtfstueckgruppe.setColumns(10);
 		jtfreferenz.setColumns(10);
 
+		
 		// JTextfield schrift festlegen
-		jtfvorname.setFont(ftfield);
-		jtfname.setFont(ftfield);
-		jtfvorname.setFont(ftfield);
-		jtftitel.setFont(ftfield);
-		jtfvorsatz.setFont(ftfield);
-		jtfzusatz.setFont(ftfield);
 
-		jtfdob.setFont(ftfield);
-		jtfdod.setFont(ftfield);
-		jtfpseudonym.setFont(ftfield);
-		jtfinstrument.setFont(ftfield);
-		jtfsolostueck.setFont(ftfield);
+		jtfname.setFont(ftfield);
+		jtfmitglied.setFont(ftfield);
+		jtfehemalig.setFont(ftfield);
+		jtfstueckgruppe.setFont(ftfield);
 		jtfreferenz.setFont(ftfield);
+		
+
+
 
 		// JTextfields hinzufuegen
 
 		jpmaininput.add(jtfname);
-		jpmaininput.add(jtfvorname);
-		jpmaininput.add(jtftitel);
-		jpmaininput.add(jtfvorsatz);
-		jpmaininput.add(jtfzusatz);
-		jpmaininput.add(geschlechtgruppe);
-		jpmaininput.add(jtfdob);
-		jpmaininput.add(jtfdod);
-		jpmaininput.add(jtfpseudonym);
-		jpmaininput.add(jtfinstrument);
-		jpmaininput.add(jtfsolostueck);
+		jpmaininput.add(jtfmitglied);
+		jpmaininput.add(jtfehemalig);
+		jpmaininput.add(jtfstueckgruppe);
 		jpmaininput.add(jtfreferenz);
+		
+		
 
 		return jpmaininput;
 
