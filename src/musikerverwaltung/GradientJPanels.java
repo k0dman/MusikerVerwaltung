e2a0b07854c3d3d930a348bcb551c792e91ffbe8
k0dman <k0dman@stars.ms>
2014-05-16 23:07:14 +0200

    ko

--- src/musikerverwaltung/GradientJPanels.java
+++ src/musikerverwaltung/GradientJPanels.java
@@ -33,32 +33,52 @@ public final class GradientJPanels extends JPanel {
 		case "gpheaderright":
 			gp = new GradientPaint(getWidth() / 2, 0, Color.DARK_GRAY,
 					getWidth() / 2, getHeight(), Color.LIGHT_GRAY);
+			g2d.setPaint(gp);
+
+			g2d.fillOval(0, -180, getWidth(), getHeight()*9);
 			break;
+
 		case "gpheaderleft":
 			gp = new GradientPaint(getWidth() / 2, 0, Color.DARK_GRAY,
 					getWidth() / 2, getHeight(), Color.LIGHT_GRAY);
+			g2d.setPaint(gp);
+
+			g2d.fillRect(0, 0, getWidth(), getHeight());
 			break;
 
 		// diagonaler Verlauf von links oben nach rechts unten
 		case "gpcenter":
 			gp = new GradientPaint(0, 0, Color.DARK_GRAY, getWidth(),
 					getHeight(), Color.LIGHT_GRAY);
+			g2d.setPaint(gp);
+
+			g2d.fillRoundRect(0, 0, getWidth(), getHeight(), 50, 50);
 			break;
+			
 		// vertikaler Verlauf von rechts oben nach links unten
 		case "gpinfo":
 			gp = new GradientPaint(0, getHeight() / 2, Color.LIGHT_GRAY,
 					getWidth(), getHeight() / 2, Color.DARK_GRAY);
+			g2d.setPaint(gp);
+
+			g2d.fillRect(0, 0, getWidth(), getHeight());
 			break;
 
 		// horizontaler, wiederholter Verlauf
 		case "gpnew":
 			gp = new GradientPaint(0, getHeight() / 2, Color.DARK_GRAY,
 					getWidth(), getHeight() / 2, Color.LIGHT_GRAY);
+			g2d.setPaint(gp);
+
+			g2d.fillRect(0, 0, getWidth(), getHeight());
 			break;
 
 		case "gpfooter":
 			gp = new GradientPaint(0, getHeight() / 2, Color.LIGHT_GRAY, 0,
 					getHeight(), Color.DARK_GRAY);
+			g2d.setPaint(gp);
+
+			g2d.fillRect(0, 0, getWidth(), getHeight());
 			break;
 		}
 
