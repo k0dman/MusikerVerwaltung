56976940155a282f866aebb3a46603ba1b8605e3
k0dman <k0dman@stars.ms>
2014-05-16 22:56:18 +0200

    jojo

--- src/musikerverwaltung/GradientJPanels.java
+++ src/musikerverwaltung/GradientJPanels.java
@@ -30,56 +30,40 @@ public final class GradientJPanels extends JPanel {
 		switch (jpanel) {
 
 		// vertikaler Verlauf
-		case "gpheaderleft":
-			gp = new GradientPaint(getWidth() / 2, 0, Color.DARK_GRAY,
-					getWidth() / 2, getHeight(), Color.LIGHT_GRAY);
-			g2d.setPaint(gp);
-
-			g2d.fillOval(0, -170, getWidth(), getHeight() * 9);
-			break;
-		
 		case "gpheaderright":
 			gp = new GradientPaint(getWidth() / 2, 0, Color.DARK_GRAY,
 					getWidth() / 2, getHeight(), Color.LIGHT_GRAY);
-			g2d.setPaint(gp);
-
-			g2d.fillOval(0, -170, getWidth(), getHeight() * 9);
+			break;
+		case "gpheaderleft":
+			gp = new GradientPaint(getWidth() / 2, 0, Color.DARK_GRAY,
+					getWidth() / 2, getHeight(), Color.LIGHT_GRAY);
 			break;
 
 		// diagonaler Verlauf von links oben nach rechts unten
 		case "gpcenter":
 			gp = new GradientPaint(0, 0, Color.DARK_GRAY, getWidth(),
 					getHeight(), Color.LIGHT_GRAY);
-			g2d.setPaint(gp);
-
-			g2d.fillRoundRect(0, 0, getWidth(), getHeight(), 5, 5);
 			break;
 		// vertikaler Verlauf von rechts oben nach links unten
 		case "gpinfo":
 			gp = new GradientPaint(0, getHeight() / 2, Color.LIGHT_GRAY,
 					getWidth(), getHeight() / 2, Color.DARK_GRAY);
-			g2d.setPaint(gp);
-
-			g2d.fillRoundRect(0, 0, getWidth(), getHeight(), 5, 5);
 			break;
 
 		// horizontaler, wiederholter Verlauf
 		case "gpnew":
 			gp = new GradientPaint(0, getHeight() / 2, Color.DARK_GRAY,
 					getWidth(), getHeight() / 2, Color.LIGHT_GRAY);
-			g2d.setPaint(gp);
-
-			g2d.fillRoundRect(0, 0, getWidth(), getHeight(), 5, 5);
 			break;
 
 		case "gpfooter":
 			gp = new GradientPaint(0, getHeight() / 2, Color.LIGHT_GRAY, 0,
 					getHeight(), Color.DARK_GRAY);
-			g2d.setPaint(gp);
-
-			g2d.fillRoundRect(0, 0, getWidth(), getHeight(), 5, 5);
 			break;
 		}
 
+		g2d.setPaint(gp);
+
+		g2d.fillRect(0, 0, getWidth(), getHeight());
 	}
 }
