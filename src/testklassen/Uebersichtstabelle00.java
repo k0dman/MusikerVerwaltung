6ed567539ac97d5d846bb805d6ee040706e5dad8
k0dman <k0dman@stars.ms>
2014-05-23 12:19:18 +0200

    hjk

--- src/testklassen/Uebersichtstabelle00.java
+++ src/testklassen/Uebersichtstabelle00.java
@@ -1,9 +1,9 @@
 package testklassen;
 
 import java.awt.*;
+import java.util.*;
 
 import javax.swing.*;
-
 import javax.swing.table.DefaultTableModel;
 
 public class Uebersichtstabelle00 extends JPanel {
@@ -45,13 +45,45 @@ public class Uebersichtstabelle00 extends JPanel {
 		// Spaltenueberschriften aus der Methode-Klasse holen
 		dtm.setColumnIdentifiers(DBMethods00.COLUMN_IDENTIFIERS);
 
+	
+
+	
 		// JTable erzeugen
 		jtmaintable = new JTable(dtm);
 
+		jtmaintable.setCellSelectionEnabled(true);
+		ListSelectionModel cellSelectionModel = jtmaintable.getSelectionModel();
+		cellSelectionModel
+				.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+		/*
+		 * cellSelectionModel .addListSelectionListener(new
+		 * ListSelectionListener() { public void valueChanged(ListSelectionEvent
+		 * e) { String selectedData = null;
+		 * 
+		 * int[] selectedRow = jtmaintable.getSelectedRows(); int[]
+		 * selectedColumns = jtmaintable .getSelectedColumns();
+		 * 
+		 * for (int i = 0; i < selectedRow.length; i++) { for (int j = 0; j <
+		 * selectedColumns.length; j++) { selectedData = (String)
+		 * jtmaintable.getValueAt( selectedRow[i], selectedColumns[j]); } }
+		 * System.out.println("Selected: " + selectedData); }
+		 * 
+		 * });
+		 */
+
 		// JScrollPane erstellen
+		jtmaintable.setAutoResizeMode(HEIGHT);
+		
 		jspmaintable = new JScrollPane(jtmaintable);
 
 		// JScrollPane dem JPanel hinzufuegem
+
+		// Groesse der Tabelle festlegen, das sonst keinen Scrollen vorhanden
+		// ist, außerdem schoener:)
+		jspmaintable.setPreferredSize(new Dimension(260, 150));
+
+		jspmaintable.setViewportView(jtmaintable);
 
 		jpmaindesc.add(jspmaintable);
 
