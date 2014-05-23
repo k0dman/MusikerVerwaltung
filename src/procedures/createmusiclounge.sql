7c3b8ee5b24b8b7c2d488484bbb18308208cd1a5
brandtphl <brandt.phl@gmail.com>
2014-05-23 12:10:23 +0200

    Sql überholt

--- src/procedures/createmusiclounge.sql
+++ src/procedures/createmusiclounge.sql
@@ -49,6 +49,28 @@ Primary Key (idgruppe)
 
 ALTER TABLE `musiker` add foreign key (idgruppe) references gruppe (idgruppe) on update cascade on delete cascade;
 
+use musiclounge;
 
+CREATE PROCEDURE DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `musikerErstellen` (musvorname varchar(100), musname varchar(100),
+mustitel varchar(100), musvorsatz varchar(100), muszusatz varchar(100),
+musgeschlecht varchar(1), musgtag varchar(100), musgmonat varchar(100), musgjahr varchar(100),
+musttag varchar(100), mustmonat varchar(100), mustjahr varchar(100),
+pseudonym varchar(100), instrument varchar(100), stuecksolo varchar(100),
+referenz varchar(100), idperson integer)
+BEGIN
+declare idperson integer;
+declare idmusiker integer;
+insert into person (name, vorname, titel, vorsatz, zusatz,
+geschlecht, gtag, gmonat, gjahr, ttag, tmonat, tjahr)
+values (musname, musvorname, mustitel, musvorsatz, muszusatz,
+musgeschlecht, musgtag, musgmonat, musgjahr, musttag, mustmonat, mustjahr);
+select last_insert_id() into idperson;
+insert into musiker (idperson, pseudonym, instrument, stuecksolo,
+referenz, idmusiker)
+values (idperson, pseudonym, instrument, stuecksolo,
+referenz, idmusiker);
 
+END$$
+DELIMITER ;
 
--- src/procedures/musikerErstellen.sql
+++ src/procedures/musikerErstellen.sql
@@ -1,24 +1,67 @@
 use musiclounge;
 
-CREATE PROCEDURE DELIMITER $$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `musikerErstellen` (musvorname varchar(100), musname varchar(100),
-mustitel varchar(100), musvorsatz varchar(100), muszusatz varchar(100),
-musgeschlecht varchar(1), musgtag varchar(100), musgmonat varchar(100), musgjahr varchar(100),
-musttag varchar(100), mustmonat varchar(100), mustjahr varchar(100),
-pseudonym varchar(100), instrument varchar(100), stuecksolo varchar(100),
-referenz varchar(100), idperson integer)
-BEGIN
-declare idperson integer;
-declare idmusiker integer;
-insert into person (name, vorname, titel, vorsatz, zusatz,
-geschlecht, gtag, gmonat, gjahr, ttag, tmonat, tjahr)
-values (musname, musvorname, mustitel, musvorsatz, muszusatz,
-musgeschlecht, musgtag, musgmonat, musgjahr, musttag, mustmonat, mustjahr);
-select last_insert_id() into idperson;
-insert into musiker (idperson, pseudonym, instrument, stuecksolo,
-referenz, idmusiker)
-values (idperson, pseudonym, instrument, stuecksolo,
-referenz, idmusiker);
+SET @p0='Philip';
+SET @p1=' Collins';
+SET @p2=''; SET @p3=''; SET @p4='';
+SET @p5='m';
+SET @p6='30';
+SET @p7='01';
+SET @p8='1951';
+SET @p9=''; SET @p10=''; SET @p11='';
+SET @p12='Phil Collins';
+SET @p13='Schlagzeug';
+SET @p14='In the Air Tonight';
+SET @p15='https://www.youtube.com/watch?v=manxPVTLth8';
+SET @p16='';
 
-END$$
-DELIMITER ;
+CALL `musikerErstellen`(@p0, @p1, @p2, @p3, @p4, @p5, @p6, @p7, @p8, @p9, @p10, @p11, @p12, @p13, @p14, @p15, @p16);
+
+SET @p0='Michael';
+SET @p1='Jagger';
+SET @p2=''; SET @p3=''; SET @p4='';
+SET @p5='m';
+SET @p6='26';
+SET @p7='07';
+SET @p8='1943';
+SET @p9=''; SET @p10=''; SET @p11='';
+SET @p12='Mick Jagger';
+SET @p13='';
+SET @p14='Just Another Night';
+SET @p15='https://www.youtube.com/watch?v=hev2qx1y2IU';
+SET @p16='';
+
+CALL `musikerErstellen`(@p0, @p1, @p2, @p3, @p4, @p5, @p6, @p7, @p8, @p9, @p10, @p11, @p12, @p13, @p14, @p15, @p16);
+
+SET @p0='Freddie';
+SET @p1='Mercury';
+SET @p2=''; SET @p3=''; SET @p4='';
+SET @p5='m';
+SET @p6='05';
+SET @p7='09';
+SET @p8='1946';
+SET @p9=''; SET @p10=''; SET @p11='';
+SET @p12='Freddie Mercury';
+SET @p13='';
+SET @p14='Living On My Own';
+SET @p15='https://www.youtube.com/watch?v=o-urnGgFs4k';
+SET @p16='';
+
+CALL `musikerErstellen`(@p0, @p1, @p2, @p3, @p4, @p5, @p6, @p7, @p8, @p9, @p10, @p11, @p12, @p13, @p14, @p15, @p16);
+
+SET @p0='Eithne';
+SET @p1='Bhraonáin';
+SET @p2=''; SET @p3=''; SET @p4='';
+SET @p5='f';
+SET @p6='17';
+SET @p7='05';
+SET @p8='1961';
+SET @p9=''; SET @p10=''; SET @p11='';
+SET @p12='Enya';
+SET @p13='';
+SET @p14='Only Time';
+SET @p15='https://www.youtube.com/watch?v=ERFmHwYrw1A';
+SET @p16='';
+
+CALL `musikerErstellen`(@p0, @p1, @p2, @p3, @p4, @p5, @p6, @p7, @p8, @p9, @p10, @p11, @p12, @p13, @p14, @p15, @p16);
+
+
