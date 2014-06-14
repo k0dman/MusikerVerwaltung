2ab74df27513148f029424ec02bd74a0e750198a
k0dman <k0dman@stars.ms>
2014-06-14 14:31:49 +0200

    Update2

--- testklassen/musikerverwaltung/Swing/AddTabs01.java
+++ testklassen/musikerverwaltung/Swing/AddTabs01.java
@@ -5,27 +5,27 @@ import javax.swing.*;
 import vorlagen.EingabeformularBand04;
 import musikerverwaltung.Graphics.Icons;
 
-public class AddTabs01 extends JTabbedPane {
-	private EingabeformularArtist04 jpmainartist = new EingabeformularArtist04();
-	private EingabeformularBand05 jpmainband = new EingabeformularBand05();
-	private AnzeigeFormularArtist00 jpmainshowartist = new AnzeigeFormularArtist00();
+public final class AddTabs01 extends JTabbedPane {
+	private static EingabeformularArtist04 jpmainartist = new EingabeformularArtist04();
+	private static EingabeformularBand05 jpmainband = new EingabeformularBand05();
+	private static AnzeigeFormularArtist00 jpmainshowartist = new AnzeigeFormularArtist00();
 	
-	private Icons tabicons = new Icons();
+	private static Icons tabicons = new Icons();
 	
-	public void showArtist(Object artist, Object title, JTabbedPane jtpane) {
+	public static void showArtist(Object artist, Object title, JTabbedPane jtpane) {
 		
 	
 		jtpane.addTab(String.valueOf(artist), tabicons.icons[2], jpmainshowartist.jpmainArtist(artist));
 		
 	}
 
-	public void insertArtist(JTabbedPane jtpane, String name) {
+	public static void insertArtist(JTabbedPane jtpane, String name) {
 
 		jtpane.addTab(name, tabicons.icons[2], jpmainartist.jpmainArtist());
 
 	}
 
-	public void insertBand(JTabbedPane jtpane, String name) {
+	public static void insertBand(JTabbedPane jtpane, String name) {
 
 		jtpane.addTab(name, tabicons.icons[1], jpmainband.jpmainBand());
 
